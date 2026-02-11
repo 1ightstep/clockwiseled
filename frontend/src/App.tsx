436cef0967cea0dc7ed8fa435062b85ba39a1a43
kAvaras <93688603+kAvaras@users.noreply.github.com>
2026-02-10 22:09:47 -0800

    feat: removed auth and backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,4 @@
-import { ProtectedRoute } from "@/components/ProtectedRoute";
 import { Toast } from "@/components/Toast";
-import { AuthPage } from "@/pages/AuthPage";
 import { DashboardPage } from "@/pages/DashboardPage";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
@@ -11,11 +9,8 @@ function App() {
       <div className="app-shell">
         <Toast />
         <Routes>
-          <Route path="/auth" element={<AuthPage />} />
-          <Route element={<ProtectedRoute />}>
-            <Route path="/dashboard" element={<DashboardPage />} />
-            <Route path="/" element={<Navigate to="/dashboard" replace />} />
-          </Route>
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/" element={<Navigate to="/dashboard" replace />} />
           <Route path="*" element={<Navigate to="/dashboard" replace />} />
         </Routes>
       </div>
